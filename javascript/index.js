eb06fae6a9c51b85070d24bedd4e51d7fac313a8
Lasse <lass876q@stud.kea.dk>
2021-11-30 22:10:54 +0100

    Menu done and style for mobile,workingontransition

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,5 @@
+document.querySelector("#openicon").addEventListener("click", addHeight);
 
+function addHeight() {
+  document.querySelector("#dropdown-menu").classList.toggle("additionalHeight");
+}
