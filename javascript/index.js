7f4ce1fbf30b0b0f3be86776a1419712eb9aedd0
Lasse <lass876q@stud.kea.dk>
2021-12-02 21:41:40 +0100

    large changes has been made, too few commits

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,19 @@
-document.querySelector("#openicon").addEventListener("click", addHeight);
+document.querySelector(".visiblemenu").addEventListener("click", addWidth);
 
-function addHeight() {
-  document.querySelector("#dropdown-menu").classList.toggle("additionalHeight");
+function addWidth() {
+  document.querySelector("#dropdown-menu").classList.toggle("additionalWidth");
+  document.querySelector(".visiblemenu").classList.toggle("additionalOpacity");
+}
+
+/* ----------------Burgermenu animation------------------- */
+document
+  .querySelector(".visiblemenu")
+  .addEventListener("click", burgermenuAnimation);
+
+function burgermenuAnimation() {
+  console.log("mojn");
+  document.querySelector(".one").classList.toggle("lineOne");
+  document.querySelector(".two").classList.toggle("lineTwo");
+  document.querySelector(".three").classList.toggle("lineThree");
+  document.querySelector(".visiblemenu").classList.toggle("additionPadding");
 }
