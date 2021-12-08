13b6acbc9b251138cd39160ccaad5df32aa01019
Lasse <lass876q@stud.kea.dk>
2021-12-08 14:08:59 +0100

    abouttomakegreatchanges

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,11 @@ document.querySelector(".visiblemenu").addEventListener("click", addWidth);
 function addWidth() {
   document.querySelector("#dropdown-menu").classList.toggle("additionalWidth");
   document.querySelector(".visiblemenu").classList.toggle("additionalOpacity");
+  document.querySelector(".visiblemenu").classList.toggle("additionalMargin");
+  document.querySelector(".visiblemenu").classList.remove(".visiblemenu:hover");
+  document
+    .querySelector(".welcome")
+    .classList.toggle("additionalOpacityWelcome");
 }
 
 /* ----------------Burgermenu animation------------------- */
@@ -16,4 +21,28 @@ function burgermenuAnimation() {
   document.querySelector(".two").classList.toggle("lineTwo");
   document.querySelector(".three").classList.toggle("lineThree");
   document.querySelector(".visiblemenu").classList.toggle("additionPadding");
+}
+/* ---------------Media queries------------------ */
+
+const mediaQuery = window.matchMedia("(max-width: 700px)");
+if (mediaQuery.matches) {
+  document.querySelector(".visiblemenu").addEventListener("click", addWidth2);
+
+  console.log("Mediaquery");
+}
+
+function addWidth2() {
+  document.querySelector("#dropdown-menu").classList.toggle("additionalWidth2");
+  document.querySelector(".visiblemenu").classList.toggle("additionalMargin2");
+}
+
+/* const mediaQuery2 = window.matchMedia("(min-width: 900px)");
+if (mediaQuery2.matches) {
+  console.log("mediaquery2");
+  document.querySelector("#background").setAttribute = ("src", "");
+  console.log("mediaquery8");
+}
+ */
+function changeBackgroundimg() {
+  /*   "../assets/homepage_backgroundimage_mobile_resized.jpg"; */
 }
