11ae4a58c07dc8d307ba698abbcda07044638fdd
Lasse <lass876q@stud.kea.dk>
2021-12-11 16:48:43 +0100

    nearing the end for landingpage

--- javascript/index.js
+++ javascript/index.js
@@ -2,48 +2,45 @@ document.querySelector(".burgericon").addEventListener("click", addWidth);
 
 /* -----------------------------Menuwidthetc--------------------------------- */
 function addWidth() {
-  console.log("eow");
-
-  document.querySelector("#dropdown-menu").classList.toggle("additionalWidth");
+  document.querySelector("#slideout-menu").classList.toggle("additionalWidth");
   document
-    .querySelector("#dropdown-menu")
+    .querySelector("#slideout-menu")
     .classList.toggle("additionalOpacity");
-  document.querySelector(".secondhalf").classList.toggle("additionalMargin");
   document
     .querySelector(".welcome")
     .classList.toggle("additionalOpacityWelcome");
-}
-
-/* ----------------Burgermenu animation------------------- */
-document
-  .querySelector(".burgericon")
-  .addEventListener("click", burgermenuAnimation);
-
-function burgermenuAnimation() {
   document.querySelector(".one").classList.toggle("lineOne");
   document.querySelector(".two").classList.toggle("lineTwo");
   document.querySelector(".three").classList.toggle("lineThree");
 }
+
+/* ----------------Burgermenu animation------------------- */
+/* document
+  .querySelector(".burgericon")
+  .addEventListener("click", burgermenuAnimation);
+
+function burgermenuAnimation() {} */
 /* ---------------Media queries------------------ */
 
 const mediaQuery = window.matchMedia("(max-width: 700px)");
 if (mediaQuery.matches) {
-  document.querySelector(".secondhalf").addEventListener("click", addWidth2);
   document
     .querySelector(".burgericon")
     .addEventListener("click", changeDisplay);
 }
 
-function addWidth2() {
-  document.querySelector(".dropdown").classList.toggle("additionalWidth2");
-}
 function changeDisplay() {
   document.querySelector(".logo-image").classList.toggle("changeOpacity");
   document.querySelector(".icons").classList.toggle("changeOpacity");
 }
+
 var bgImg = document.querySelector("#bg");
+
 const mediaQuery2 = window.matchMedia("(min-width: 700px)");
 if (mediaQuery2.matches) {
   console.log("query2");
   bgImg.setAttribute("src", "assets/bgimg_desktop.jpg");
+
+  document.querySelector(".asidecontainer").addEventListener("click", addWidth);
+  document.querySelector(".secondhalf").addEventListener("click", addWidth);
 }
