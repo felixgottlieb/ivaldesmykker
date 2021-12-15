b10ef6425a9e9bf41e820940c49dd9cab40c05a8
Lasse <lass876q@stud.kea.dk>
2021-12-15 15:20:45 +0100

    yupyup

---
+++ javascript/products_listed.js
@@ -0,0 +1,53 @@
+document.querySelector(".burgericon").addEventListener("click", addWidth);
+
+/* -----------------------------Menuwidthetc--------------------------------- */
+function addWidth() {
+  document.querySelector("#slideout-menu").classList.toggle("additionalWidth");
+  document
+    .querySelector("#slideout-menu")
+    .classList.toggle("additionalOpacity");
+
+  document.querySelector(".logo-image").classList.toggle("noClickEvents");
+  document.querySelector(".icons").classList.toggle("noClickEvents");
+  document.querySelector(".one").classList.toggle("lineOne");
+  document.querySelector(".two").classList.toggle("lineTwo");
+  document.querySelector(".three").classList.toggle("lineThree");
+}
+
+/* ---------------Media queries------------------ */
+
+const mediaQuery = window.matchMedia("(max-width: 700px)");
+if (mediaQuery.matches) {
+  document
+    .querySelector(".burgericon")
+    .addEventListener("click", changeDisplay);
+}
+
+function changeDisplay() {
+  document.querySelector(".logo-image").classList.toggle("changeOpacity");
+  document.querySelector(".icons").classList.toggle("changeOpacity");
+}
+
+const mediaQuery2 = window.matchMedia("(min-width: 700px)");
+if (mediaQuery2.matches) {
+  document.querySelector(".asidecontainer").addEventListener("click", addWidth);
+  document.querySelector(".secondhalf").addEventListener("click", addWidth);
+  document
+    .querySelector(".asidecontainer")
+    .addEventListener("click", LogoandIconOpac);
+  document
+    .querySelector(".secondhalf")
+    .addEventListener("click", LogoandIconOpac);
+  document
+    .querySelector(".burgericon")
+    .addEventListener("click", LogoandIconOpac);
+
+  function LogoandIconOpac() {
+    document
+      .querySelector(".logo-image")
+      .classList.toggle("additionalOpacityWelcome");
+    document
+      .querySelector(".icons")
+      .classList.toggle("additionalOpacityWelcome");
+  }
+}
